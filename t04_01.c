4fa79dcd9bc97eb2ad8393fe3f6108aa327bd734
GriseldaHutahaean <griseldatabita@gmail.com>
2025-02-27 13:47:05 +0700

    PAIR 20

--- t04_01.c
+++ t04_01.c
@@ -1,10 +1,25 @@
-// NIM - Name
-// NIM - Name
+// 12S24011 - Pedro Simangunsong 
+// 12S24026 - Griselda Tabitha Nathania Hutahaean
 
 #include <stdio.h>
+#include <string.h>
 
 int main(int _argv, char **_argc) {
-  // codes
+   char numbs[255];
+
+   scanf("%s", numbs);
+
+   int length = strlen(numbs);
+   for (int i = 0; i<length; i+=3)
+   {
+    int number = 0;
+    for (int x = 0; x < 3 && (x+i) < length; x++)
+    {
+      number = number * 10 + (numbs[x+i]-'0');
+    }
+    printf("%c", number);
+   }
+
 
   return 0;
 }
--- t04_02.c
+++ t04_02.c
@@ -1,10 +1,18 @@
-// NIM - Name
-// NIM - Name
+// 12S24011 - Pedro Simangunsong 
+// 12S24026 - Griselda Tabitha Nathania Hutahaean
 
 #include <stdio.h>
+#include <string.h>
 
 int main(int _argv, char **_argc) {
-  // codes
+  char string [21];
+  scanf("%20[^\n]", string);
+  int length = strlen(string);
+  for (int i = 0; i < length; i++) {
+  printf ("%03d", string[i]);
+  }
 
+printf("\n");
+  
   return 0;
 }
